9278d3019afa933c75d82478efdcf609f06c7e95
arashad202 <ahm.rashad.202@gmail.com>
2025-07-05 04:52:55 +0300

    Create recursive fibsRec

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,14 @@ function fibs(n) {
 }
 
 console.log(fibs(8));
+
+function fibsRec(n) {
+  if (n <= 0) return [0];
+  if (n === 2) return [0, 1];
+
+  const arr = fibsRec(n - 1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return arr;
+}
+
+console.log(fibsRec(8));
